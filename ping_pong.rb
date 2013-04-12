3d4ae712c50c4cc259f6664ceedfd2ba53056b7f
Nathan Wallace <nathan.m.wallace@gmail.com>
2013-04-11 21:23:48 -0400

    assignment 1

---
+++ ping_pong.rb
@@ -0,0 +1,29 @@
+# written in Ruby 2.0.0p0 on MRI, Mac OS X 10.7
+
+require 'thread'
+
+sounds = ["Ping!", "Pong!"]
+threads = []
+iterations = 3
+
+mx = Mutex.new
+cv = ConditionVariable.new
+
+puts "Ready... Set... Go!"
+
+sounds.each do |sound|
+	threads << Thread.new do
+		iterations.times do |i|
+			mx.synchronize do
+				puts sound
+				cv.signal
+				cv.wait(mx) unless i == iterations - 1
+			end
+		end
+	end
+end
+
+threads.each { |t| t.join }
+
+puts "Done!"
+
